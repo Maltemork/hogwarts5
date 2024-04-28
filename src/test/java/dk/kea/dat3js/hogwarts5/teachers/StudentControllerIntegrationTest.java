e4e402f5dacbc24f710489d7a0468c579aec7fbf
Maltemork <malte.sorensen@protonmail.com>
2024-04-28 17:06:17 +0200

    created integration test for student with controller.

--- src/test/java/dk/kea/dat3js/hogwarts5/teachers/StudentControllerIntegrationTest.java
+++ src/test/java/dk/kea/dat3js/hogwarts5/teachers/StudentControllerIntegrationTest.java
@@ -1,5 +1,44 @@
 package dk.kea.dat3js.hogwarts5.teachers;
 
-public class StudentControllerIntegrationTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentControllerIntegrationTest {
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    void notNull() {
+        assertNotNull(webClient);
+    }
+
+    @Test
+    void createStudentWithFullName() {
+        webClient
+                .post().uri("/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                {
+                    "name": "Peter Heronimous Lind",
+                    "house": "Gryffindor",
+                    "schoolYear": 7
+                }
+                """)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.id").exists()
+                .jsonPath("$.firstName").isEqualTo("Peter")
+                .jsonPath("$.middleName").isEqualTo("Heronimous")
+                .jsonPath("$.lastName").isEqualTo("Lind")
+                .jsonPath("$.house").isEqualTo("Gryffindor")
+                .jsonPath("$.schoolYear").isEqualTo(7);
+    }
 }
